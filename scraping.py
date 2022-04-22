1c7eab4bc1e205eadff4115c327296a363ee48c5
Tony <98559889+Acromic@users.noreply.github.com>
2022-04-22 00:58:06 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -101,33 +101,3 @@ if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
-
-    # ### Featured Image
-    # Visit URL
-    # url = 'https://spaceimages-mars.com'
-    # browser.visit(url)
-
-    # # Find and click the full image button
-    # full_image_elem = browser.find_by_tag('button')[1]
-    # full_image_elem.click()
-
-    # # Parse the resulting html with soup
-    # html = browser.html
-    # img_soup = soup(html, 'html.parser')
-
-    # # Find the relative image url
-    # img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-    # img_url_rel
-
-    # # Use the base URL to create an absolute URL
-    # img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-    # img_url
-
-    # df = pd.read_html('https://galaxyfacts-mars.com')[0]
-    # df.columns=['description', 'Mars', 'Earth']
-    # df.set_index('description', inplace=True)
-    # df
-
-    # df.to_html()
-
-    # browser.quit()
